88226bef1914dd03e219053c8008765e4ab03e91
niedjo <niedjokuitche@gmail.com>
2025-07-22 11:52:27 +0100

    first commit

---
+++ app/(root)/create-thread/page.tsx
@@ -0,0 +1,20 @@
+import PostThread from "@/components/forms/PostThread"
+import { fetchUser } from "@/lib/actions/user.actions"
+import { currentUser } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation"
+
+const Page = async () => {
+    const user = await currentUser()
+    const userInfo = await fetchUser(user?.id as string)
+    console.log(JSON.stringify(userInfo?._id))
+    
+    if(!userInfo?.onboarded) redirect('/onboarding');
+    return (
+        <>
+            <h1 className='head-text'>Create Thread</h1>
+            <PostThread userId={userInfo?._id} />
+        </>
+    )
+}
+
+export default Page
